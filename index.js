2b63ddac0e102f092e5d48900b4182833c33a42d
Fabian-LG <100322206+Fabian-LG@users.noreply.github.com>
2023-04-10 20:34:49 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
-const testimonials = [
-    {
-        name: "Annamarie W.",
-        photoUrl: "https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfDJ8MHx8&auto=format&fit=crop&w=900&q=60",
-        text: "Not able to tell you how happy I am with apple. You guys rock! It fits our needs perfectly."
-    },
-    {
-        name: "Evette D.",
-        photoUrl: "https://images.unsplash.com/photo-1516695554846-842fc913388e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1481&q=80",
-        text: "We've seen amazing results already. Apple has got everything I need. It really saves me time and effort. apple is exactly what our business has been lacking."
-    },
-    {
-        name: "Tome B.",
-        photoUrl: "https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
-        text: "Dude, your stuff is the bomb! Thank you so much for your help. No matter where you go, apple is the coolest, most happening thing around! I can't say enough about apple."
-    }
-];
+const monthE1 = document.querySelector(".date h1");
+const fullDateE1 = document.querySelector(".date p");
+const daysE1 = document.querySelector(".days");
 
-const imgE1 = document.querySelector("img");
-const textE1 = document.querySelector(".text");
-const usernameE1 = document.querySelector(".username");
+const monthInx = new Date().getMonth();
 
-let index = 0;
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay();
+console.log("El firstDay es: " + firstDay);
 
-updateTestimonial();
 
-function updateTestimonial(){
-    const {name, photoUrl, text} = testimonials[index];
-    imgE1.src = photoUrl;
-    textE1.innerText = text;
-    usernameE1.innerText = name;
-    
-    index++;
 
-    if(index===testimonials.length){
-        index=0;
-    }
-    setTimeout(()=>{
-        updateTestimonial()
-    }, 2000);
+
+const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+]
+
+console.log(months[monthInx])
+
+monthE1.innerText = months[monthInx];
+fullDateE1.innerText = new Date().toDateString();
+
+let days ="";
+
+for(let i=firstDay; i>0;i--){
+    days+=`<div class="empty"></div>`;
 }
+
+for(let i=1; i<=lastDay; i++){
+    if(i=== new Date().getDate()){
+        days += `<div class="today">${i}</div>`;
+    } else{
+        days+=`<div>${i}</div>`;
+    }
+}
+
+daysE1.innerHTML = days;
